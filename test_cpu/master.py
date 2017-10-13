88afbe6104723161cae9291d83af68b4f792a14e
Mikael söderberg <mikael.misod@gmail.com>
2017-10-13 14:39:54 +0200

    gårdagens ändringar

--- test_cpu/master.py
+++ test_cpu/master.py
@@ -3,7 +3,7 @@ app = Flask("master")
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, Slave!!!'
 
 @app.route('/slave/<int:slave_id>')
 def get_info(slave_id):
@@ -17,7 +17,10 @@ def get_info_cpu(slave_id, cpu):
     print slave_id
     print cpu
 
-    return jsonify("recived")
+    #see if it is time for a scale up
+    #call webhook to activate action
+
+    return jsonify(1)
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0', debug=True)
--- test_cpu/slave.py
+++ test_cpu/slave.py
@@ -1,13 +1,36 @@
-import requests, flask, psutil, time
+import requests, flask, psutil, time, re, signal, sys
 
 from random import randint
-UUID = randint(0, 10000)
-
+UUID = randint(0, 100000)
+patToFile = "file_ip.txt"
+fileStream = open(patToFile, 'r')
 UUID_str = str(UUID)
-url = 'http://130.239.81.63:5000/slave/'+UUID_str+'/'
-for i in range(100):
-    cpu = psutil.cpu_percent(interval=1, percpu=True)
-    cpu_in_string = str(cpu[0])
+ip_to_master = ''
+
+try:
+    with fileStream as openFile:
+        for line in openFile:
+            if line != '\n':
+                ip_to_master = re.sub('[^0-9.]+', '', line)
+except Exception as e:
+    print "nu blev det GALET"
+    print e
+finally:
+    fileStream.close()
+
+def signal_handler(signal, frame):
+        print('quitting')
+        time.sleep(0.5)
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+url = 'http://'+ip_to_master+':5000/slave/'+UUID_str+'/'
+
+for i in range(5):
+    cpu = psutil.cpu_percent(interval=5, percpu=False)
+    cpu_in_string = str(cpu)
     r = requests.get(url+cpu_in_string)
     response_json = r.json()
     print response_json
