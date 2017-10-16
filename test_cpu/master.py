6ca85ef066b529acf937ac2985f56d33849b67e5
Mikael söderberg <mikael.misod@gmail.com>
2017-10-16 11:42:17 +0200

    test

--- test_cpu/master.py
+++ test_cpu/master.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import time
 app = Flask("master")
+dir_files = 'files_info/'
 
 @app.route('/')
 def hello_world():
@@ -11,9 +12,11 @@ def get_info(slave_id, action):
     if action == 'Q':
         print 'quitting slave'
         print slave_id
+        #remove file in dir
     elif action == 'N':
         print 'new user'
         print slave_id
+        #create file in dir
     else:
         print 'not a good input'
         return jsonify(0)
@@ -25,10 +28,12 @@ def get_info(slave_id, action):
 def get_info_cpu(slave_id, cpu):
     print slave_id
     print cpu
+    #update files
 
     #see if it is time for a scale up
+
     #call webhook to activate action
-    time.delay(4)
+
     return jsonify(1)
 
 if __name__ == '__main__':
