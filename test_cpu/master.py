bb4dfba1fe84fb72b6f9d29c7397b0b30dc0b888
Mikael söderberg <mikael.misod@gmail.com>
2017-10-16 13:36:35 +0200

    saftey

--- test_cpu/master.py
+++ test_cpu/master.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-import time
+import time, os
 app = Flask("master")
 dir_files = 'files_info/'
 
@@ -7,19 +7,42 @@ dir_files = 'files_info/'
 def hello_world():
     return 'Hello, Slave!'
 
-def create_file():
+def check_writable(path):
+    try:
+        return os.access(path, os.W_OK)
+    except Exception as e:
+        print 'not allowed to write in folder'
+        print e
+    return False
 
-    return
+def file_exists(path_file):
+        return os.path.exists(path_file)
+
+def create_file(slave_id, cpu):
+    file_status = file_exists(path+slave_id)
+    if check_writable(dir_files) and not file_status:
+        try:
+            f = open('myfile', 'w')
+            f.write('hi there\n')  # python will convert \n to os.linesep
+            f.close()
+            return True
+        except Exception as e:
+            print 'wrong in create_file() ----> error'
+            print e
+    elif file_status:
+        return False
+
+    return False
 
 def read_files():
 
     return
 
-def update_file():
+def update_file(slave_id, cpu):
 
     return
 
-def delete_file():
+def delete_file(slave_id):
 
     return
 
@@ -45,12 +68,15 @@ def get_info_cpu(slave_id, cpu):
     print slave_id
     print cpu
 
-    if
-    #update files
+    if update_file(slave_id, cpu) is None:
+        print "it went crazy ", slave_id, cpu
+        return 0
 
-    #see if it is time for a scale up
-
-    #call webhook to activate action
+    info_slaves = read_files()
+    if info_slaves is not None:
+        #iterate over array and se if it is time for a scaleing
+        #call webhook to activate action
+        return
 
     return jsonify(1)
 
